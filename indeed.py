2058e80699c1d9913557f6a4e9a4148fa4c623c5
skakanka <kondracka.anna@gmail.com>
2017-02-05 21:00:38 -0800

    individual job descriptions printing

--- indeed.py
+++ indeed.py
@@ -4,6 +4,8 @@ import os
 import requests
 import json
 import math
+from bs4 import BeautifulSoup
+
 
 treat = os.environ["PUB_ID"]
 
@@ -16,8 +18,8 @@ URL = "http://api.indeed.com/ads/apisearch?"
 
 params = {
     'publisher' : treat,
-    'q' : "junior software engineer python",
-    'l' : "san francisco",
+    'q' : "junior software developer OR intern",
+    'l' : "san francisco bay area, CA",
     'userip' : "192.168.1.165",
     'useragent' : "Chrome/55.0.2883.95",
     'v' : 2,
@@ -30,22 +32,34 @@ total_results = response.json()['totalResults']
 total_pages = math.ceil(int(response.json()['totalResults'])/25.)
 print total_results
 print total_pages
-print int(response.json()['totalResults'])/25.
-print response.json()['pageNumber']
+# print int(response.json()['totalResults'])/25.
 print 
+
 csv_file = open("indeed_job_apps.csv", "w") 
 
 fieldnames = ["jobtitle","company","city","state","country",
 "formattedLocation","source","date","snippet","url","onmousedown",
 "latitude","longitude","jobkey","sponsored","expired",
-"formattedLocationFull","formattedRelativeTime","stations","indeedApply"]
+"formattedLocationFull","formattedRelativeTime","stations","indeedApply","job_summary"]
 writer = csv.DictWriter(csv_file, dialect='excel', fieldnames=fieldnames)
 writer.writeheader()
-for page in xrange(0, int(total_pages)):
-    response_pages = requests.get(response.url, params={"page":page})
-    print response_pages
 
+# going through all pages
+for page in xrange(0, int(total_pages)):
+    print response.json()['pageNumber']
+    response_pages = requests.get(response.url, params={"page":page})
+    
+    # results per page
     for r in response_pages.json()['results']:
+        response_job = requests.get(r['url'])
+        soup = BeautifulSoup(response_job.text, 'html.parser')
+        desc2 = soup.find('span', attrs={'id': 'job_summary'}).get_text()
+
+        print desc2.encode('utf-8')
+        
+
+
+
         writer.writerow(dict((k, v.encode('utf-8') if type(v) is unicode else v) for k, v in r.iteritems()))
 
 csv_file.close()
