450b6acd7d6bfbea1230040ef83b718a17009daa
skakanka <kondracka.anna@gmail.com>
2017-02-06 23:16:01 -0800

    pandas dataframe with all job data plus desc

--- indeed.py
+++ indeed.py
@@ -5,6 +5,7 @@ import requests
 import json
 import math
 from bs4 import BeautifulSoup
+import pandas as pd
 
 
 treat = os.environ["PUB_ID"]
@@ -35,33 +36,41 @@ print total_pages
 # print int(response.json()['totalResults'])/25.
 print 
 
-csv_file = open("indeed_job_apps.csv", "w") 
+csv_file = open("indeed_job_apps.csv", "a") 
 
-fieldnames = ["jobtitle","company","city","state","country",
+
+fieldnames = ["jobtitle","compan","city","state","country",
 "formattedLocation","source","date","snippet","url","onmousedown",
 "latitude","longitude","jobkey","sponsored","expired",
 "formattedLocationFull","formattedRelativeTime","stations","indeedApply","job_summary"]
 writer = csv.DictWriter(csv_file, dialect='excel', fieldnames=fieldnames)
 writer.writeheader()
 
+df = pd.DataFrame(columns=fieldnames)
+i = 0
 # going through all pages
 for page in xrange(0, int(total_pages)):
-    print response.json()['pageNumber']
+    # print "PAGE",response.json()['pageNumber']
     response_pages = requests.get(response.url, params={"page":page})
-    
+    print "PAGE",page
     # results per page
     for r in response_pages.json()['results']:
+        # print r
         response_job = requests.get(r['url'])
         soup = BeautifulSoup(response_job.text, 'html.parser')
         desc2 = soup.find('span', attrs={'id': 'job_summary'}).get_text()
+        desc2 = desc2.encode('utf-8')
+        # print desc2
 
-        print desc2.encode('utf-8')
+        # print desc2.encode('utf-8')
+        df = df.append(r, ignore_index=True)
+        df.iloc[i,-2] = desc2
+        i += 1
+        # print df.head()
+
+        # writer.writerow(dict((k, v.encode('utf-8') if type(v) is unicode else v) for k, v in r.iteritems()))
         
-
-
-
-        writer.writerow(dict((k, v.encode('utf-8') if type(v) is unicode else v) for k, v in r.iteritems()))
-
+print df.head()
 csv_file.close()
 
 
